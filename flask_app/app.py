aee33c20a3f1a184af966105d664af687baf4624
oneytlam <lam.yikting@dhs.sg>
2022-05-05 21:10:35 +0800

    minor-update

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return "<h1>Hello world!</h1>"
+    return render_template('index.html')
+  
+  
+@app.route('/navpage')
+def navpage():
+    return render_template('navpage.html')
+  
+  
+@app.route('/gfg/<int:page>')
+def gfg(page):
+    return render_template('gfg.html', page=page)
+  
+  
+if __name__ == '__main__':
+    app.run()
